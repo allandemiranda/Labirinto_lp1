4845acdb44be578fc67485219657b67f1bb24ba8
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-11-25 15:55:00 -0300

    class maze implementada

--- include/maze.h
+++ include/maze.h
@@ -1,83 +1,64 @@
+/**
+ * @file maze.h
+ * @author Allan de Miranda - Josoé Cláudio
+ * @brief Classe Maze que irá guardar o controle lógico do labirinto
+ * @version 0.1
+ * @date 2018-11-25
+ * 
+ * @copyright Copyright (c) 2018
+ * 
+ */
+
 #ifndef MAZE_H
 #define MAZE_H
 
-#include <iostream>
-#include <memory>  // unique_ptr<>
-#include <cstring> // memset, memcpy
-#include <vector>
+#include <vector>   // std::vector
 
-namespace maze {
-    /**
-     * @brief Classe que representa o labirinto
-     * 
-     */
-    class Maze
-    {
-        public:            
-            /**
-             * @brief Estrutura que representa uma celula do labirinto
-             * 
-             */
-            struct celula 
-            {
-                enum tipo { LIVRE, ENTRADA, SAIDA, CAMINHO, CAMINHO_DESCARTADO };
-                enum estados { VISITADA, NAO_VISITADA, FAZ_PARTE_DO_CAMINHO, NAO_FAZ_PARTE_DO_CAMINHO };
-                bool walls[4] = {1,1,1,1}; /// paredes norte, sul, leste, oeste
-            };
-            //=== Alias
-            using maze_matrix = std::vector<std::vector<celula> >; /// apelido para a matrix que representa o labirinto
+/**
+ * @brief Class Maze do Labirinto
+ * 
+ */
+class maze
+{
+    private:
+        /**
+         * @brief Estrutura lógica de uma célula do labirinto
+         * 
+         */
+        struct celula
+        {
+            int tipo = 0;                   //! Tipo da célula livre = 0, entrada = 1, saída = 2, caminho = 3, caminho_descartado = 4   
+            bool paredes[4] = {1,1,1,1};    //! Paredes norte, sul, leste, oeste da célula
+        };
+        /**
+         * @brief Dados lógico do labirinto
+         * 
+         */
+        int m_linhas;                               //! Quantidade de linhas
+        int m_colunas;                              //! Quantidade de colunas        
+        std::vector <std::vector <celula>> m_maze;  //! Matriz que guarda as células
+        int posicao_atual[2] = {0,0};               //! Célula que se encontra o personagem
+        celula status_celula(int, int);             //! Status da célula
 
-            //=== Special members
-            Maze( void )
-            { //FAZER ESSA PARTE ALLAN
-                for(auto i(0u); i<m_rows; ++i){
-                    std::vector <celula> temp;
-                    for(auto j(0u); j<m_columns; ++j){
-                        //nova_celula celula;
-                        //nova_celula.tipo(LIVRE)
-                        //temp.push_back();                                           
-                    }
-                }
-                //m_maze[0][0].ENTRADA;
-               // m_maze[m_rows-1][m_columns-1].SAIDA;
-            }
+    public:
+        maze(int, int);        
+        bool status_celula_livre(int, int);
+        bool status_celula_entrada(int, int);
+        bool status_celula_saida(int, int);
+        bool status_celula_caminho(int, int);
+        bool status_celula_caminho_descartado(int, int);
+        bool status_celula_parede_norte(int, int);
+        bool status_celula_parede_sul(int, int);
+        bool status_celula_parede_leste(int, int);
+        bool status_celula_parede_oeste(int, int);
+        void modificar_para_entrada(int, int);
+        void modificar_para_saida(int, int);
+        void modificar_para_caminho(int, int);
+        void modificar_para_caminho_descartado(int, int);
+        void derrubar_parede_norte(int, int);
+        void derrubar_parede_sul(int, int);
+        void derrubar_parede_leste(int, int);
+        void derrubar_parede_oeste(int, int);
+};
 
-            /// Destructor.
-            virtual ~Maze( void )
-            {
-                // TODO 
-            }
-
-            //=== Special members
-            /// Copy constructor.
-            Maze( const Maze & );
-            /// Assignment operator.
-            Maze & operator=( const Maze & );
-
-            //=== Members
-            void set_rows( int r ) { m_rows = r; }
-            int get_rows() { return m_rows; }
-
-            void set_columns( int c ) { m_columns = c; }
-            int get_columns() { return m_columns; }
-
-            void set_width( size_t w ) { m_width = w; }
-            int get_width() { return m_width; }
-
-            void set_height( size_t h ) { m_height = h; }
-            size_t get_height() { return m_height; }
-
-            /// para pegar o labirinto
-            maze_matrix get_maze() { return m_maze; }
-            
-        private:
-            //=== Members
-            maze_matrix m_maze; /// matrix que representa o labirinto
-            int m_rows; /// linhas de m_maze
-            int m_columns; /// colunas de m_maze
-            size_t m_width;
-            size_t m_height;
-    };
-} // namespace
-
-#endif // MAZE_H
+#endif
